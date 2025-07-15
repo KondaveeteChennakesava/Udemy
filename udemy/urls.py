ffd56b57acec3d4f24e7de1f847b4819d886207c
KondaveeteChennakesava <chennakesavakondaveete@gmail.com>
2025-07-15 18:33:34 +0530

    urls added

--- udemy/urls.py
+++ udemy/urls.py
@@ -18,12 +18,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-
-from .views import landing
+from courses.views import landing_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', landing, name='landing'),
+    path('', landing_page, name='landing'),
     path('accounts/', include('accounts.urls')),
     path('instructor/', include('instructor.urls')),
+    path('courses/', include('courses.urls')),
+    path('enrollments/', include('enrollments.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
