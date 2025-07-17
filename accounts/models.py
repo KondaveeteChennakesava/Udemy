5be6f443a91caf7def7c7904349ba8feaadc88a4
KondaveeteChennakesava <chennakesavakondaveete@gmail.com>
2025-07-17 16:45:35 +0530

    models modified

--- accounts/models.py
+++ accounts/models.py
@@ -14,6 +14,15 @@ class User(AbstractUser):
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     profile_picture = models.ImageField(upload_to=user_profile_upload_path, blank=True, null=True)
+    bio = models.TextField(blank=True)
+    followers = models.ManyToManyField('User', related_name='following_instructors', blank=True)
+    location = models.CharField(max_length=100, blank=True, null=True)
+    website = models.URLField(blank=True, null=True)
+    social_github = models.URLField(blank=True, null=True)
+    social_linkedin = models.URLField(blank=True, null=True)
 
     def __str__(self):
         return f"{self.user.username}'s profile"
+
+    def total_followers(self):
+        return self.followers.count()
