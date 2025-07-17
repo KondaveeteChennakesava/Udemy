1ecce55897a8c5b749b2ec486cced528fb5fad2a
KondaveeteChennakesava <chennakesavakondaveete@gmail.com>
2025-07-17 16:46:27 +0530

    enrollment option added

--- enrollments/views.py
+++ enrollments/views.py
@@ -7,4 +7,4 @@ from django.contrib.auth.decorators import login_required
 def enroll_course(request, course_id):
     course = get_object_or_404(Course, pk=course_id)
     Enrollment.objects.get_or_create(student=request.user, course=course)
-    return redirect('courses:course_dashboard', pk=course_id)
+    return redirect('courses:course_detail', pk=course_id)
